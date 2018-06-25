2e5fca48abcc9d0d6298165c7a4fd44a867d19b9
CAP\Nemo <nemoicap@gmail.com>
2018-06-25 13:52:51 +0300

    Init WPF app

---
+++ BuildAndDeployApp/BuildAndDeployApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace BuildAndDeployApp
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var applicationDirectory = directoryPath.Text;
+            var isDirectoryExists = Directory.Exists(applicationDirectory);
+            if (!isDirectoryExists)
+            {
+                Directory.CreateDirectory(applicationDirectory);
+            }
+
+            var msBuildCommand =
+                $"msbuild {applicationDirectory} \"/p:Platform=AnyCPU;Configuration=Release;PublishDestination=E:\\batches\\Apps\\WebApp\" /t:PublishToFileSystem";
+
+            var msBuildPathVariable = Environment.GetEnvironmentVariable("msBuildPath");
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo(msBuildPathVariable)
+                {
+                    Arguments = msBuildCommand
+                }
+            };
+
+            process.Start();
+
+
+        }
+    }
+}
