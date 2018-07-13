e2b9fc84eafcecc4230ae10247472f9fa3ce2b67
CAP\Nemo <nemoicap@gmail.com>
2018-07-13 10:55:28 +0300

    working example

--- BuildAndDeployApp/BuildAndDeployApp/MainWindow.xaml.cs
+++ BuildAndDeployApp/BuildAndDeployApp/MainWindow.xaml.cs
@@ -17,28 +17,52 @@ namespace BuildAndDeployApp
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            var applicationDirectory = directoryPath.Text;
-            var isDirectoryExists = Directory.Exists(applicationDirectory);
-            if (!isDirectoryExists)
+            try
             {
-                Directory.CreateDirectory(applicationDirectory);
-            }
-
-            var msBuildCommand =
-                $"msbuild {applicationDirectory} \"/p:Platform=AnyCPU;Configuration=Release;PublishDestination=E:\\batches\\Apps\\WebApp\" /t:PublishToFileSystem";
-
-            var msBuildPathVariable = Environment.GetEnvironmentVariable("msBuildPath");
-            var process = new Process
-            {
-                StartInfo = new ProcessStartInfo(msBuildPathVariable)
+                var msBuildPath = "";
+                if (!string.IsNullOrEmpty(msBuildInput.Text))
                 {
-                    Arguments = msBuildCommand
+                    msBuildPath = Environment.GetEnvironmentVariable(msBuildInput.Text);
+                    if (string.IsNullOrEmpty(msBuildPath))
+                        msBuildPath = msBuildInput.Text;
                 }
-            };
+                else
+                {
+                    MessageBox.Show("Kindly Set MSBuild Path or Envirement Variable with Direction Path");
+                    return;
+                }
 
-            process.Start();
+                var applicationDirectory = directoryPath.Text;
+                var isDirectoryExists = Directory.Exists(applicationDirectory);
+                if (!isDirectoryExists)
+                    Directory.CreateDirectory(applicationDirectory);
+                var msBuildCommand =
+                    $"msbuild {applicationPath.Text} /p:Platform=AnyCPU;Configuration=Release;PublishDestination={applicationDirectory} /t:PublishToFileSystem";
 
 
+                var pStartInfo =
+                    new ProcessStartInfo("cmd.exe",
+                        @"/k ""cd /d " + msBuildPath + @"""")
+                    {
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false
+                    };
+
+                var process = Process.Start(pStartInfo);
+                if (process != null)
+                {
+                    process.StandardInput.WriteLine(msBuildCommand);
+                    process.StandardInput.Close();
+                    var outputString = process.StandardOutput.ReadToEnd();
+                    MessageBox.Show(outputString);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
         }
     }
 }
